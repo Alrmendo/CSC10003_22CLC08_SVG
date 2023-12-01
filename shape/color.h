7e7c52eb678836c3a6b729e974480b0ea54aa976
altheatomic <128811814+altheatomic@users.noreply.github.com>
2023-12-01 20:39:20 +0700

    Add named colors and hex colors.

--- shape/color.h
+++ shape/color.h
@@ -1,15 +1,21 @@
 #ifndef __COLOR__
 #define __COLOR__
+
 #include <string>
 #include <sstream>
+#include <unordered_map>
 using namespace std;
+
 class Color
 {
 public:
     int alpha, red, green, blue;
     Color();
+    Color(int, int, int);
     Color(int, int, int, int);
     Color(const string &, const string &);
     Color(const string &);
+    Color(const string &, unordered_map<string, string> &);
+    void hex_to_rgb(const string &);
 };
 #endif

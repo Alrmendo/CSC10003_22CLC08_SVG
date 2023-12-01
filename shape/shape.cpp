e249c18faa4f0ae9e236f8fb5373267dce8734c9
tienloi244 <139633455+tienloi244@users.noreply.github.com>
2023-12-02 05:06:09 +0700

    apply transform and implement stroke text

--- shape/shape.cpp
+++ shape/shape.cpp
@@ -40,7 +40,6 @@ Shape::Shape(Entity entity)
         }
     }
 }
-
 void Shape::apply_transform(Gdiplus::Graphics &graphics)
 {
     for (const auto &transform : this->transforms)
@@ -59,143 +58,187 @@ void Shape::apply_transform(Gdiplus::Graphics &graphics)
     }
 }
 
+// Rectangle Class
 Rectangle::Rectangle(Entity entity) : Shape(entity)
 {
-    this->x = stof(entity.attributes["x"]);
-    this->y = stof(entity.attributes["y"]);
-    this->width = stof(entity.attributes["width"]);
-    this->height = stof(entity.attributes["height"]);
+    this->x = entity.attributes.find("x") != entity.attributes.end() ? stof(entity.attributes["x"]) : 0;
+    this->y = entity.attributes.find("y") != entity.attributes.end() ? stof(entity.attributes["y"]) : 0;
+    this->width = entity.attributes.find("width") != entity.attributes.end() ? stof(entity.attributes["width"]) : 0;
+    this->height = entity.attributes.find("height") != entity.attributes.end() ? stof(entity.attributes["height"]) : 0;
 }
-void Rectangle::render_rect(Gdiplus::Graphics &graphics)
+void Rectangle::render(Gdiplus::Graphics &graphics)
 {
-    Gdiplus::Pen stroke_pen(Gdiplus::Color(this->stroke_color.alpha, this->stroke_color.red, this->stroke_color.green, this->stroke_color.blue), this->stroke_width);
+    Gdiplus::GraphicsState state = graphics.Save();
+    this->apply_transform(graphics);
+
     Gdiplus::SolidBrush fill_pen(Gdiplus::Color(this->fill_color.alpha, this->fill_color.red, this->fill_color.green, this->fill_color.blue));
     Gdiplus::RectF shape(this->x, this->y, this->width, this->height);
     graphics.FillRectangle(&fill_pen, shape);
+
     if (this->stroke_width != 0)
+    {
+        Gdiplus::Pen stroke_pen(Gdiplus::Color(this->stroke_color.alpha, this->stroke_color.red, this->stroke_color.green, this->stroke_color.blue), this->stroke_width);
         graphics.DrawRectangle(&stroke_pen, shape);
+    }
+    graphics.Restore(state);
 }
 
+// Text Class
 Text::Text(Entity entity) : Shape(entity)
 {
-    this->x = stof(entity.attributes["x"]);
-    this->y = stof(entity.attributes["y"]);
-    this->font_size = stoi(entity.attributes["font-size"]);
-    if (!this->font_family.empty())
-        this->font_family = entity.attributes["font-family"];
-    else
-        this->font_family = "Times New Roman";
-    this->content = entity.content;
+    this->x = entity.attributes.find("x") != entity.attributes.end() ? stof(entity.attributes["x"]) : 0;
+    this->y = entity.attributes.find("y") != entity.attributes.end() ? stof(entity.attributes["y"]) : 0;
+    this->font_size = entity.attributes.find("font-size") != entity.attributes.end() ? stoi(entity.attributes["font-size"]) : 0;
+    this->font_family = entity.attributes.find("font-family") != entity.attributes.end() ? entity.attributes["font-family"] : "times new roman";
+    this->text_anchor = entity.attributes.find("text-anchor") != entity.attributes.end() ? entity.attributes["text-anchor"] : "start";
+    this->font_style = entity.attributes.find("font-style") != entity.attributes.end() ? entity.attributes["font-style"] : "normal";
+    this->text_content = entity.text_content;
+    offset_map = {
+        {"start", -0.15 * this->font_size},
+        {"middle", 0},
+        {"end", 0.15 * this->font_size},
+    };
 }
-void Text::render_text(Gdiplus::Graphics &graphics)
+void Text::render(Gdiplus::Graphics &graphics)
 {
-    Gdiplus::SolidBrush fill_pen(Gdiplus::Color(this->fill_color.alpha, this->fill_color.red, this->fill_color.green, this->fill_color.blue));
-    wstring wide_font_family(this->font_family.begin(), this->font_family.end());
+    Gdiplus::GraphicsState state = graphics.Save();
+    this->apply_transform(graphics);
+
+    wstring wide_content = wstring(this->text_content.begin(), this->text_content.end());
+    wstring wide_font_family = wstring(this->font_family.begin(), this->font_family.end());
+
     Gdiplus::FontFamily font_family(wide_font_family.c_str());
-    Gdiplus::Font font(&font_family, this->font_size, Gdiplus::FontStyleRegular, Gdiplus::UnitPixel);
-    Gdiplus::PointF point_f(this->x, this->y - this->font_size);
-    wstring wide_content(this->content.begin(), this->content.end());
-    graphics.DrawString(wide_content.c_str(), -1, &font, point_f, &fill_pen);
+    Gdiplus::FontStyle font_style = this->font_style_map[this->font_style];
+
+    Gdiplus::PointF origin(this->x, this->y - 0.9 * this->font_size);
+    origin.X += offset_map[this->text_anchor];
+
+    Gdiplus::StringFormat format(Gdiplus::StringFormat::GenericDefault());
+    format.SetAlignment(this->text_anchor_map[this->text_anchor]);
+
+    Gdiplus::GraphicsPath path;
+    path.AddString(wide_content.c_str(), -1, &font_family, font_style, this->font_size, origin, &format);
+
+    Gdiplus::SolidBrush fill_pen(Gdiplus::Color(this->fill_color.alpha, this->fill_color.red, this->fill_color.green, this->fill_color.blue));
+    graphics.FillPath(&fill_pen, &path);
+
+    if (this->stroke_width != 0)
+    {
+        Gdiplus::Pen stroke_pen(Gdiplus::Color(this->stroke_color.alpha, this->stroke_color.red, this->stroke_color.green, this->stroke_color.blue), this->stroke_width);
+        graphics.DrawPath(&stroke_pen, &path);
+    }
+    graphics.Restore(state);
 }
 
+// Ellipse Class
 Ellipse::Ellipse(Entity entity) : Shape(entity)
 {
-    this->cx = stof(entity.attributes["cx"]);
-    this->cy = stof(entity.attributes["cy"]);
-    this->rx = stof(entity.attributes["rx"]);
-    this->ry = stof(entity.attributes["ry"]);
+    this->cx = entity.attributes.find("cx") != entity.attributes.end() ? stof(entity.attributes["cx"]) : 0;
+    this->cy = entity.attributes.find("cy") != entity.attributes.end() ? stof(entity.attributes["cy"]) : 0;
+    this->rx = entity.attributes.find("rx") != entity.attributes.end() ? stof(entity.attributes["rx"]) : 0;
+    this->ry = entity.attributes.find("ry") != entity.attributes.end() ? stof(entity.attributes["ry"]) : 0;
 }
-void Ellipse::render_ellipse(Gdiplus::Graphics &graphics)
+void Ellipse::render(Gdiplus::Graphics &graphics)
 {
-    Gdiplus::Pen stroke_pen(Gdiplus::Color(this->stroke_color.alpha, this->stroke_color.red, this->stroke_color.green, this->stroke_color.blue), this->stroke_width);
+    Gdiplus::GraphicsState state = graphics.Save();
+    this->apply_transform(graphics);
+
     Gdiplus::SolidBrush fill_pen(Gdiplus::Color(this->fill_color.alpha, this->fill_color.red, this->fill_color.green, this->fill_color.blue));
     Gdiplus::RectF shape(this->cx - this->rx, this->cy - this->ry, this->rx * 2, this->ry * 2);
     graphics.FillEllipse(&fill_pen, shape);
+
     if (this->stroke_width != 0)
+    {
+        Gdiplus::Pen stroke_pen(Gdiplus::Color(this->stroke_color.alpha, this->stroke_color.red, this->stroke_color.green, this->stroke_color.blue), this->stroke_width);
         graphics.DrawEllipse(&stroke_pen, shape);
+    }
+    graphics.Restore(state);
 }
 
-Circle::Circle(Entity entity) : Shape(entity)
+// Circle Class
+Circle::Circle(Entity entity) : Ellipse(entity)
 {
-    this->cx = stof(entity.attributes["cx"]);
-    this->cy = stof(entity.attributes["cy"]);
-    this->r = stof(entity.attributes["r"]);
-}
-void Circle::render_circle(Gdiplus::Graphics &graphics)
-{
-    Gdiplus::Pen stroke_pen(Gdiplus::Color(this->stroke_color.alpha, this->stroke_color.red, this->stroke_color.green, this->stroke_color.blue), this->stroke_width);
-    Gdiplus::SolidBrush fill_pen(Gdiplus::Color(this->fill_color.alpha, this->fill_color.red, this->fill_color.green, this->fill_color.blue));
-    Gdiplus::RectF shape(this->cx - this->r, this->cy - this->r, this->r * 2, this->r * 2);
-    graphics.FillEllipse(&fill_pen, shape);
-    if (this->stroke_width != 0)
-        graphics.DrawEllipse(&stroke_pen, shape);
+    this->rx = entity.attributes.find("r") != entity.attributes.end() ? stof(entity.attributes["r"]) : 0;
+    this->ry = entity.attributes.find("r") != entity.attributes.end() ? stof(entity.attributes["r"]) : 0;
 }
 
+// Line Class
 Line::Line(Entity entity) : Shape(entity)
 {
-    this->x1 = stof(entity.attributes["x1"]);
-    this->y1 = stof(entity.attributes["y1"]);
-    this->x2 = stof(entity.attributes["x2"]);
-    this->y2 = stof(entity.attributes["y2"]);
+    this->x1 = entity.attributes.find("x1") != entity.attributes.end() ? stof(entity.attributes["x1"]) : 0;
+    this->y1 = entity.attributes.find("y1") != entity.attributes.end() ? stof(entity.attributes["y1"]) : 0;
+    this->x2 = entity.attributes.find("x2") != entity.attributes.end() ? stof(entity.attributes["x2"]) : 0;
+    this->y2 = entity.attributes.find("y2") != entity.attributes.end() ? stof(entity.attributes["y2"]) : 0;
 }
-void Line::render_line(Gdiplus::Graphics &graphics)
+void Line::render(Gdiplus::Graphics &graphics)
 {
+    Gdiplus::GraphicsState state = graphics.Save();
+    this->apply_transform(graphics);
+
     Gdiplus::Pen stroke_pen(Gdiplus::Color(this->stroke_color.alpha, this->stroke_color.red, this->stroke_color.green, this->stroke_color.blue), this->stroke_width);
     graphics.DrawLine(&stroke_pen, this->x1, this->y1, this->x2, this->y2);
+    graphics.Restore(state);
 }
 
-Polyline::Polyline(Entity entity) : Shape(entity)
+// PolyBase Class
+PolyBase::PolyBase(Entity entity) : Shape(entity)
 {
-    stringstream ss(entity.attributes["points"]);
-    string point;
-
-    while (getline(ss, point, ' '))
+    stringstream point_stream(entity.attributes["points"]);
+    float x, y;
+    while (point_stream >> x)
     {
-        size_t comma_postion = point.find(',');
-        if (comma_postion != string::npos)
-        {
-            float x = stof(point.substr(0, comma_postion));
-            float y = stof(point.substr(comma_postion + 1));
-            this->points.push_back({x, y});
-        }
+        point_stream >> y;
+        this->points.push_back({x, y});
     }
 }
-void Polyline::render_polyline(Gdiplus::Graphics &graphics)
+
+// Polyline Class
+Polyline::Polyline(Entity entity) : PolyBase(entity) {}
+void Polyline::render(Gdiplus::Graphics &graphics)
 {
-    Gdiplus::Pen stroke_pen(Gdiplus::Color(this->stroke_color.alpha, this->stroke_color.red, this->stroke_color.green, this->stroke_color.blue), this->stroke_width);
-    Gdiplus::SolidBrush fill_pen(Gdiplus::Color(this->fill_color.alpha, this->fill_color.red, this->fill_color.green, this->fill_color.blue));
+    Gdiplus::GraphicsState state = graphics.Save();
+    this->apply_transform(graphics);
+
+    Gdiplus::SolidBrush fill_pen(Gdiplus::Color(fill_color.alpha, fill_color.red, fill_color.green, fill_color.blue));
     Gdiplus::GraphicsPath path;
-    for (size_t i = 0; i < this->points.size() - 1; i += 1)
-        path.AddLine(this->points[i].first, this->points[i].second, this->points[i + 1].first, this->points[i + 1].second);
+    for (size_t i = 0; i < points.size() - 1; i += 1)
+        path.AddLine(points[i].first, points[i].second, points[i + 1].first, points[i + 1].second);
     graphics.FillPath(&fill_pen, &path);
-    if (this->stroke_width != 0)
-        graphics.DrawPath(&stroke_pen, &path);
-}
 
-Polygon::Polygon(Entity entity) : Shape(entity)
-{
-    stringstream ss(entity.attributes["points"]);
-    string point;
-
-    while (getline(ss, point, ' '))
+    if (stroke_width != 0)
     {
-        size_t comma_postion = point.find(',');
-        if (comma_postion != string::npos)
-        {
-            float x = stof(point.substr(0, comma_postion));
-            float y = stof(point.substr(comma_postion + 1));
-            this->points.push_back({x, y});
-        }
+        Gdiplus::Pen stroke_pen(Gdiplus::Color(stroke_color.alpha, stroke_color.red, stroke_color.green, stroke_color.blue), stroke_width);
+        graphics.DrawPath(&stroke_pen, &path);
     }
+    graphics.Restore(state);
 }
-void Polygon::render_polygon(Gdiplus::Graphics &graphics)
+
+// Polygon Class
+Polygon::Polygon(Entity entity) : PolyBase(entity) {}
+void Polygon::render(Gdiplus::Graphics &graphics)
 {
-    Gdiplus::Pen stroke_pen(Gdiplus::Color(this->stroke_color.alpha, this->stroke_color.red, this->stroke_color.green, this->stroke_color.blue), this->stroke_width);
-    Gdiplus::SolidBrush fill_pen(Gdiplus::Color(this->fill_color.alpha, this->fill_color.red, this->fill_color.green, this->fill_color.blue));
-    vector<Gdiplus::PointF> points;
-    for (const auto &point : this->points)
-        points.push_back(Gdiplus::PointF(point.first, point.second));
-    graphics.FillPolygon(&fill_pen, points.data(), points.size());
-    if (this->stroke_width != 0)
-        graphics.DrawPolygon(&stroke_pen, points.data(), points.size());
+    Gdiplus::GraphicsState state = graphics.Save();
+    this->apply_transform(graphics);
+
+    vector<Gdiplus::PointF> gdiplus_points;
+    for (const auto &point : points)
+        gdiplus_points.push_back(Gdiplus::PointF(point.first, point.second));
+
+    Gdiplus::SolidBrush fill_pen(Gdiplus::Color(fill_color.alpha, fill_color.red, fill_color.green, fill_color.blue));
+    graphics.FillPolygon(&fill_pen, gdiplus_points.data(), gdiplus_points.size(), Gdiplus::FillModeWinding);
+
+    if (stroke_width != 0)
+    {
+        Gdiplus::Pen stroke_pen(Gdiplus::Color(stroke_color.alpha, stroke_color.red, stroke_color.green, stroke_color.blue), stroke_width);
+        graphics.DrawPolygon(&stroke_pen, gdiplus_points.data(), gdiplus_points.size());
+    }
+    graphics.Restore(state);
+}
+
+// Path Class
+Path::Path(Entity entity) : Shape(entity)
+{
+}
+void Path::render(Gdiplus::Graphics &graphics)
+{
 }

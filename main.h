a417365a89b1ac17e3ff0c412e29426bac193870
tienloi244 <139633455+tienloi244@users.noreply.github.com>
2023-12-01 18:43:43 +0700

    update main.h, add global variables for rotate and camera

--- main.h
+++ main.h
@@ -2,17 +2,25 @@
 #define __MAIN__
 #include "shape/shape.h"
 #include "reader/read_file.h"
+#include <Windowsx.h>
 
-double zoom_scale = 1;
-double rotation_angle = 0;
-int max_rotations = 4;
-int rotation_count = 0;
-Gdiplus::PointF scroll_offset(0, 0);
-Gdiplus::PointF rotate_offset(0, 0);
-string file_name = "images/sample.svg";
-vector<Entity> main_data;
+void render_shape(Gdiplus::Graphics &, Entity &);
+void render_file(HDC &, Entity &);
 
-VOID render_file(HDC &, const vector<Entity> &);
 LRESULT CALLBACK HandleMessage(HWND, UINT, WPARAM, LPARAM);
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, INT);
+
+// Global variables for render
+string filename = "images/sample.svg";
+SvgParser svg_parser(filename);
+Entity svg_data;
+
+// Navigation
+double zoom_scale = 1;
+Gdiplus::PointF scroll_offset(0, 0);
+double rotation_angle = 0;
+POINT last_mouse_position;
+bool is_dragging = false;
+int camera_speed = 48;
+int rotate_speed = 15;
 #endif

7907c594e8dc55a25b6a5a0b149729e08f6d8cca
Alrmendo <ntriet1606@gmail.com>
2023-12-15 09:57:52 +0700

    handle segment type h

--- shape/shape.cpp
+++ shape/shape.cpp
@@ -381,5 +381,15 @@ void Path::render(Gdiplus::Graphics &graphics)
                 }
             }
         }
+        
+        else if (segment.type == 'h')
+        {
+            for (size_t i = 0; i < segment.points.size(); i += 1)
+            {
+                Gdiplus::PointF end_point(current_point.X + segment.points[i], current_point.Y);
+                path.AddLine(current_point, end_point);
+                current_point = end_point;
+            }
+        }
     }
 }

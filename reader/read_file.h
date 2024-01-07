6bbeb232bfc2dade18f7315d08c3e6e4b4128e04
tienloi244 <tienloi2125@gmail.com>
2024-01-07 11:22:06 +0700

    add attributes for gradient

--- reader/read_file.h
+++ reader/read_file.h
@@ -13,9 +13,15 @@
 using namespace std;
 using namespace rapidxml;
 
+struct Transform
+{
+    string type;
+    vector<float> values;
+};
+
 struct Entity
 {
-    int depth;
+    int depth = 0;
     string type;
     string text_content;
     unordered_map<string, string> attributes;
@@ -26,18 +32,26 @@ class SvgParser
 {
 private:
     Entity data;
+    Entity defs;
     string filename;
 
 public:
-    SvgParser(const string &filename);
+    SvgParser(const string &);
     Entity get_data();
-    void traverse_node(const xml_node<> *node, Entity &entity, int depth = 0);
+    Entity get_defs();
+    void traverse_node(const xml_node<> *, Entity &, int = 0);
+    void traverse_defs(const xml_node<> *, Entity &, int = 0);
     void parse_file();
     void print_data() const;
 };
 
-string to_lower(const string &input);
-string replace_comma_with_space(const string &input);
-string add_space_between_char_and_number(const string &input);
-string format_text(const string &input);
+string to_lower(const string &);
+string replace_comma_with_space(const string &);
+string add_space_between_char_and_number(const string &);
+string format_text(const string &);
+string remove_percent(const string &);
+
+extern string filename;
+extern SvgParser svg_parser;
+extern Entity svg_data;
 #endif
